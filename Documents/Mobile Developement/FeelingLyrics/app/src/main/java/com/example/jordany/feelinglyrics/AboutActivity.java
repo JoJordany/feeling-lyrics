f895b34c52de206c80ef4084c99f6356df026f6e
JoJordany <jordany927@outlook.com>
2016-05-04 22:32:25 -0400

    First Commit!

---
+++ Documents/Mobile Developement/FeelingLyrics/app/src/main/java/com/example/jordany/feelinglyrics/AboutActivity.java
@@ -0,0 +1,24 @@
+package com.example.jordany.feelinglyrics;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+//        // my_child_toolbar is defined in the layout file
+//        Toolbar myChildToolbar =
+//                (Toolbar) findViewById(R.id.about_toolbar);
+//        setSupportActionBar(myChildToolbar);
+//
+//        // Get a support ActionBar corresponding to this toolbar
+//        ActionBar ab = getSupportActionBar();
+//
+//        // Enable the Up button
+//        ab.setDisplayHomeAsUpEnabled(true);
+    }
+}
